c05bec616e0bec68cf1030da21ba3d8f34967b02
Roopak Venkatakrishnan <roopak.v@gmail.com>
2020-06-28 15:27:06 -0700

    [Reports] Better reports on swissknife

--- src/finalize-db.ts
+++ src/finalize-db.ts
@@ -7,6 +7,7 @@ import * as configUtils from "./config-utils";
 import * as externalQueries from "./external-queries";
 import * as sharedEnv from "./shared-environment";
 import * as util from "./util";
+import _ from "lodash";
 
 /**
  * A list of queries from https://github.com/github/codeql that
@@ -231,6 +232,54 @@ async function runQueries(
   }
 }
 
+// TODO(roopakv): Make this work for bitbucket.
+function getFileUriAtCommit(uri: string, lineLoc?: string) {
+  const owner = util.getRequiredEnvParam("CIRCLE_PROJECT_USERNAME");
+  const repo = util.getRequiredEnvParam("CIRCLE_PROJECT_REPONAME");
+  const currentSha = util.getRequiredEnvParam("CIRCLE_SHA1");
+  if (!lineLoc) {
+    lineLoc = "";
+  }
+  return `https://github.com/${owner}/${repo}/blob/${currentSha}/${uri}${lineLoc}`;
+}
+
+async function fixUpReports(reportFolder: string) {
+  try {
+    const files = fs.readdirSync(reportFolder);
+    for (let report of files) {
+      if (!report || !report.endsWith(".sarif")) {
+        continue;
+      }
+      const reportString = fs.readFileSync(path.join(reportFolder, report), "utf-8");
+      const reportJson = JSON.parse(reportString);
+      // TODO(roopakv): Handle more than first run
+      const run = reportJson.runs && reportJson.runs.length > 0 && reportJson.runs[0];
+      if (!run) {
+        continue;
+      }
+      for (let result of run.results) {
+        for (let rloc of result.relatedLocations) {
+          const uri = _.get(rloc, "physicalLocation.artifactLocation.uri");
+          if (uri) {
+            _.set(rloc, "physicalLocation.artifactLocation.uri", getFileUriAtCommit(uri));
+          }
+        }
+
+        const artLoc = _.get(result, "locations.0.physicalLocation.artifactLocation");
+        const artUri = artLoc.uri;
+        let startLine = _.get(artLoc, "region.startLine");
+        startLine = startLine ? `#${startLine}` : "";
+        if (artUri) {
+          _.set(artLoc, "properties.href", getFileUriAtCommit(artUri, startLine));
+        }
+      }
+      fs.writeFileSync(path.join(reportFolder, `${report}.swissknife`), JSON.stringify(reportJson));
+    }
+  } catch (err) {
+    console.error("Couldn't build Swissknife sarif");
+  }
+}
+
 export async function run() {
   console.log("Starting Finalize");
   try {
@@ -252,6 +301,9 @@ export async function run() {
 
     console.info("Analyzing database");
     await runQueries(codeqlCmd, databaseFolder, outputFolder, config);
+
+    console.log("Fixing up reports");
+    await fixUpReports(outputFolder);
 
     const reportToSwissknife = process.env.SK_REPORT_TO_SWISSKNIFE;
     if (reportToSwissknife && reportToSwissknife === "true") {
